b76e2a7f8836b63e1e40b1f62388fd65415b0658
Samson Ocran <samson.ocran@gmail.com>
2023-07-18 03:35:08 +0100

    added styling to 404 page

--- src/pages/page-not-found/PageNotFound.js
+++ src/pages/page-not-found/PageNotFound.js
@@ -11,11 +11,11 @@ const PageNotFound = () => {
     }
   return (
     <div style={{textAlign: 'center'}}>
-        <h1>404</h1>
+        <h1 style={{marginTop: '3rem', marginBottom: '1rem'}}>404</h1>
         <img src={error404} alt='404' />
         <h2 style={{margin: '1rem 0'}}>Page Not Found.....</h2>
 
-        <Button btnText="Navigate to Home Page" clickHandler={toHomePage} />
+        <Button btnText="Back to Home Page" clickHandler={toHomePage} />
     </div>
   )
 }
